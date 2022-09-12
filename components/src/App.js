ec25b5836921cfc3743a3c63b7f70aa2a8cdf83b
Alexandra Schurman <alexandraschurman@gmail.com>
2022-09-12 11:24:34 -0700

    components hooked up

--- components/src/App.js
+++ components/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import UserLogin from './UserLogin';
+import CreateUser from './CreateUser';
+
 
 function App() {
   return (
+    <>
    <h1> Hello World</h1> 
-
+   < UserLogin /> 
+   <CreateUser /> 
+   </>
   );
 }
 
